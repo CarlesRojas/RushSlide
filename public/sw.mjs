963904129a1e5a771f5bbc9397ab4e1272496860
CarlesRojas <carles.rojas@mundimoto.com>
2022-12-29 10:32:22 +0100

    update sw

--- public/sw.mjs
+++ public/sw.mjs
@@ -1,72 +1,51 @@
-// Incrementing OFFLINE_VERSION will kick off the install event and force
-// previously cached resources to be updated from the network.
-// This variable is intentionally declared and unused.
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const OFFLINE_VERSION = 1;
-const CACHE_NAME = 'offline';
+const CACHE = "pwabuilder-offline-page"
 
-// Customize this with a different URL if needed.
-const OFFLINE_URL = 'offline.html';
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js")
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      // Setting {cache: 'reload'} in the new request ensures that the
-      // response isn't fulfilled from the HTTP cache; i.e., it will be
-      // from the network.
-      await cache.add(new Request(OFFLINE_URL, { cache: 'reload' }));
-    })(),
-  );
+const offlineFallbackPage = "offline.html"
 
-  // Force the waiting service worker to become the active service worker.
-  self.skipWaiting();
-});
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting()
+  }
+})
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    (async () => {
-      // Enable navigation preload if it's supported.
-      // See https://developers.google.com/web/updates/2017/02/navigation-preload
-      if ('navigationPreload' in self.registration) {
-        await self.registration.navigationPreload.enable();
-      }
-    })(),
-  );
+self.addEventListener("install", async (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage)))
+})
 
-  // Tell the active service worker to take control of the page immediately.
-  self.clients.claim();
-});
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable()
+}
 
-self.addEventListener('fetch', (event) => {
-  // Only call event.respondWith() if this is a navigation request
-  // for an HTML page.
-  if (event.request.mode === 'navigate') {
+workbox.routing.registerRoute(
+  new RegExp("/*"),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE,
+  }),
+)
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
     event.respondWith(
       (async () => {
         try {
-          // First, try to use the navigation preload response if it's
-          // supported.
-          const preloadResponse = await event.preloadResponse;
-          if (preloadResponse) {
-            return preloadResponse;
+          const preloadResp = await event.preloadResponse
+
+          if (preloadResp) {
+            return preloadResp
           }
 
-          // Always try the network first.
-          const networkResponse = await fetch(event.request);
-          return networkResponse;
+          const networkResp = await fetch(event.request)
+          return networkResp
         } catch (error) {
-          // catch is only triggered if an exception is thrown, which is
-          // likely due to a network error.
-          // If fetch() returns a valid HTTP response with a response code in
-          // the 4xx or 5xx range, the catch() will NOT be called.
-
-          const cache = await caches.open(CACHE_NAME);
-          const cachedResponse = await cache.match(OFFLINE_URL);
-          return cachedResponse;
+          const cache = await caches.open(CACHE)
+          const cachedResp = await cache.match(offlineFallbackPage)
+          return cachedResp
         }
       })(),
-    );
+    )
   }
-});
+})
