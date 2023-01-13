67a7404a5a21cebd97db8386a5afb93d29a95f12
CarlesRojas <carles.rojas@mundimoto.com>
2023-01-13 16:31:47 +0100

    fix landscape homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,19 +14,23 @@ const Home: NextPage = () => {
   if (!loaded) return null
 
   return (
-    <main className="relative m-auto flex h-full w-full max-w-xl flex-col items-center justify-center gap-24 overflow-hidden">
+    <main className="relative m-auto flex h-full w-full flex-col items-center justify-center gap-12 overflow-hidden lg:max-w-xl lg:gap-24 landscape:flex-row landscape:gap-4 landscape:lg:flex-col landscape:lg:gap-24">
       <div className="flex flex-col items-center justify-center gap-6">
         <Image src={"/logo.png"} alt="rush slide logo" width={80} height={80} />
-        <h1 className="text-6xl font-medium opacity-90">RUSH SLIDE</h1>
+        <h1 className="whitespace-nowrap text-6xl font-medium opacity-90 landscape:px-8 landscape:lg:px-0">
+          RUSH SLIDE
+        </h1>
       </div>
 
-      <div className="flex w-full flex-col gap-8 overflow-hidden">
-        <BoardTypeSelector />
-        <Difficulty />
-      </div>
+      <div className="flex w-full flex-col items-center justify-center gap-12 lg:gap-24">
+        <div className="flex w-full flex-col gap-8 overflow-hidden">
+          <BoardTypeSelector />
+          <Difficulty />
+        </div>
 
-      <div className="h-fit w-fit min-w-[10rem]">
-        <Anchor route={Route.GAME} icon={<RiPlayLine />} text="Play" primary />
+        <div className="h-fit w-fit min-w-[10rem]">
+          <Anchor route={Route.GAME} icon={<RiPlayLine />} text="Play" primary />
+        </div>
       </div>
     </main>
   )
